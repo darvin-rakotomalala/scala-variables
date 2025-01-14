5f69ac5c654e35eb00887d382ba32014b70e9b0e
Darvin <75081354+darvin-rakotomalala@users.noreply.github.com>
2025-01-14 18:41:41 -0500

    Delete src/main/scala directory

--- src/main/scala/MethodParamExample.scala
+++
@@ -1,8 +0,0 @@
-/* Scala program of Method scope for Scala variable
-class created with Method variables s1 and s2. */
-class MethodParamExample {
-  def multiplication(s1: Int, s2: Int): Unit = {
-    var result = s1 * s2
-    println("Area is: " + result);
-  }
-}
